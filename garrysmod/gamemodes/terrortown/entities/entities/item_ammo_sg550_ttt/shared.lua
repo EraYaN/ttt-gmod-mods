995fb43df14d103a6b30fe534031a8ff84e4c36d
robinhes@live.nl <robinhes@live.nl@8f216330-2a17-40c2-cf62-df102b661a47>
2010-12-07 19:11:32 +0000

    modified   garrysmod/gamemodes/terrortown/entities/weapons/weapon_ttt_sg550/shared.lua added      garrysmod/gamemodes/terrortown/entities/entities/item_ammo_sg550_ttt added      garrysmod/gamemodes/terrortown/entities/entities/item_ammo_sg550_ttt/shared.lua (added sg550 ammo DOES NOT WORK)

---
+++ garrysmod/gamemodes/terrortown/entities/entities/item_ammo_sg550_ttt/shared.lua
@@ -0,0 +1,19 @@
+-- SIG 550 ammo override
+
+if SERVER then
+   AddCSLuaFile( "shared.lua" )
+end
+
+ENT.Type = "anim"
+ENT.Base = "base_ammo_ttt"
+ENT.AmmoType = "sg550"
+ENT.AmmoAmount = 20
+ENT.AmmoMax = 40
+ENT.Model = Model("models/items/357ammo.mdl")
+
+function ENT:Initialize()
+    -- Differentiate from rifle ammo
+    self:SetColor(112, 138, 144, 255)
+
+    self:RealInit()
+end
