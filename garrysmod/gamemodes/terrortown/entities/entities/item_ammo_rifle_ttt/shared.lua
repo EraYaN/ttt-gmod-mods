5149987474573ea241bbab6b8688917e06bf21d7
robinhes@live.nl <robinhes@live.nl@8f216330-2a17-40c2-cf62-df102b661a47>
2010-12-10 17:57:37 +0000

    modified   garrysmod/gamemodes/terrortown/entities/entities/item_ammo_rifle_ttt/shared.lua (fixed not being able to pick up weapons which you dont have equiped)

--- garrysmod/gamemodes/terrortown/entities/entities/item_ammo_rifle_ttt/shared.lua
+++ garrysmod/gamemodes/terrortown/entities/entities/item_ammo_rifle_ttt/shared.lua
@@ -20,28 +20,26 @@ function ENT:Touch(ent)
       if (ent:IsValid() and ent:IsPlayer() and self:CheckForWeapon(ent) and self:PlayerCanPickup(ent)) then
 
          local ammo = ent:GetAmmoCount(self.AmmoType)
-		 local class = ent:GetActiveWeapon( ):GetClass()
 				
-			 if class == "weapon_ttt_ak47" and (60 >= (ammo + math.ceil(30 * 0.25))) then
+			 if ent:GetWeapon("weapon_ttt_ak47"):IsValid() and (60 >= (ammo + math.ceil(30 * 0.25))) then
 				ent:GiveAmmo( math.min(30, 60 - ammo), self.AmmoType)
 				self:Remove()
 				-- just in case remove does not happen soon enough
 				self.taken = true
 			
-		 
-				 elseif class == "weapon_ttt_m16"  and (50 >= (ammo + math.ceil(25 * 0.25))) then
+				 elseif ent:GetWeapon("weapon_ttt_m16"):IsValid() and (50 >= (ammo + math.ceil(25 * 0.25))) then
 					ent:GiveAmmo( math.min(25, 50 - ammo), self.AmmoType)
 					self:Remove()
 					-- just in case remove does not happen soon enough
 					self.taken = true
 				
-					 elseif class == "weapon_ttt_famas" and (75 >= (ammo + math.ceil(25 * 0.25))) then
+					 elseif ent:GetWeapon("weapon_ttt_famas"):IsValid() and (75 >= (ammo + math.ceil(25 * 0.25))) then
 						ent:GiveAmmo( math.min(25, 75 - ammo), self.AmmoType)
 						self:Remove()
 						-- just in case remove does not happen soon enough
 						self.taken = true
 						end	
-		 end            
+           
 		end	
 	end
 end
