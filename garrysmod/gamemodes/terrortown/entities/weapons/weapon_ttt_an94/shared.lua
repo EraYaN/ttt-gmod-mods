c851a64854fe14b6cedd26a6f3372dd24dab0471
robinhes@live.nl <robinhes@live.nl@8f216330-2a17-40c2-cf62-df102b661a47>
2011-03-25 18:25:31 +0000

    yay removed g36c backup shithing updated resource lines added buglist

--- garrysmod/gamemodes/terrortown/entities/weapons/weapon_ttt_an94/shared.lua
+++ garrysmod/gamemodes/terrortown/entities/weapons/weapon_ttt_an94/shared.lua
@@ -1,41 +1,51 @@
 -- AN-94 Abakan
 
 if SERVER then
-   AddCSLuaFile( "shared.lua" )
-   resource.AddFile("materials/VGUI/ttt/icon_bb_an94.vmt")
-   resource.AddFile("materials/models/weapons/v_models/v_rif_an94/frame.vmt")
-   resource.AddFile("materials/models/weapons/v_models/v_rif_an94/gp30.vmt")
-   resource.AddFile("materials/models/weapons/v_models/v_rif_an94/kobra.vmt")
-   resource.AddFile("materials/models/weapons/v_models/v_rif_an94/lense.vmt")
-   resource.AddFile("materials/models/weapons/v_models/v_rif_an94/mag.vmt")
-   resource.AddFile("materials/models/weapons/v_models/v_rif_an94/mount.vmt")
-   resource.AddFile("materials/models/weapons/v_models/v_rif_an94/vog25p.vmt")
-   resource.AddFile("materials/models/weapons/w_models/w_rif_an94/frame.vmt")
-   resource.AddFile("materials/models/weapons/w_models/w_rif_an94/gp30.vmt")
-   resource.AddFile("materials/models/weapons/w_models/w_rif_an94/kobra.vmt")
-   resource.AddFile("materials/models/weapons/w_models/w_rif_an94/lense.vmt")
-   resource.AddFile("materials/models/weapons/w_models/w_rif_an94/mag.vmt")
-   resource.AddFile("materials/models/weapons/w_models/w_rif_an94/mount.vmt")
-   resource.AddFile("materials/models/weapons/w_models/w_rif_an94/vog25p.vmt")
-   resource.AddFile("models/weapons/v_rif_an94.mdl")
-   resource.AddFile("models/weapons/w_rif_an94.mdl")
-   resource.AddFile("sound/weapons/an94/an94_boltpull.wav")
-   resource.AddFile("sound/weapons/an94/an94_Clipin.wav")
-   resource.AddFile("sound/weapons/an94/an94_Clipout.wav")
-   resource.AddFile("sound/weapons/an94/an94-1.wav")
-
+	AddCSLuaFile( "shared.lua" )
+	resource.AddFile("materials/VGUI/ttt/icon_bb_an94.vmt")
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\frame.vmt" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\Frame.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\Frame_normal.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\Frame_ref.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\gp30.vmt" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\gp30.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\gp30_normal.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\gp30_ref.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\kobra.vmt" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\kobra.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\kobra_normal.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\kobra_ref.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\lense.vmt" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\lense.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\mag.vmt" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\mag.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\mag_normal.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\mount.vmt" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\mount.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\mount_normal.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\mount_ref.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\vog25p.vmt" )
+	resource.AddFile( "materials\models\weapons\v_models\v_rif_an94\vog25p.vtf" )
+	resource.AddFile("models/weapons/v_rif_an94.mdl")
+	resource.AddFile("models/weapons/w_rif_an94.mdl")
+	resource.AddFile("sound/weapons/an94/an94_boltpull.wav")
+	resource.AddFile("sound/weapons/an94/an94_Clipin.wav")
+	resource.AddFile("sound/weapons/an94/an94_Clipout.wav")
+	resource.AddFile("sound/weapons/an94/an94-1.wav")
+	resource.AddFile("scripts/sounds/an94.txt")
+	resource.AddFile("scripts/game_sounds_manifest.txt")
 end
 
 SWEP.HoldType			= "ar2"
-   
+	
 
 if CLIENT then
 
-   SWEP.PrintName			= "AN-94 Abakan"			
-   SWEP.Author				= "NinjaTuna"
-   SWEP.Slot				= 2
+	SWEP.PrintName			= "AN-94 Abakan"			
+	SWEP.Author				= "NinjaTuna"
+	SWEP.Slot				= 2
 
-   SWEP.Icon = "VGUI/ttt/icon_bb_an94"
+	SWEP.Icon = "VGUI/ttt/icon_bb_an94"
 end
 
 
@@ -54,7 +64,7 @@ SWEP.Primary.Cone = 0.02
 SWEP.Primary.ClipSize = 30
 SWEP.Primary.ClipMax = 60
 SWEP.Primary.DefaultClip = 30
-SWEP.AutoSpawnable      = true
+SWEP.AutoSpawnable		= true
 SWEP.AmmoEnt = "item_ammo_rifle_ttt"
 SWEP.ViewModel			= "models/weapons/v_rif_an94.mdl"
 SWEP.WorldModel			= "models/weapons/w_rif_an94.mdl"
@@ -67,54 +77,54 @@ SWEP.IronSightsAng = Vector (-0.1223, 0.1062, 0)
 
 
 function SWEP:SetZoom(state)
-   if CLIENT then return end
-   if state then
-      self.Owner:SetFOV(35, 0.5)
-   else
-      self.Owner:SetFOV(0, 0.2)
-   end
+	if CLIENT then return end
+	if state then
+		self.Owner:SetFOV(35, 0.5)
+	else
+		self.Owner:SetFOV(0, 0.2)
+	end
 end
 
 -- Add some zoom to ironsights for this gun
 function SWEP:SecondaryAttack()
-   if not self.IronSightsPos then return end
-   if self.Weapon:GetNextSecondaryFire() > CurTime() then return end
-   
-   bIronsights = not self:GetIronsights()
-   
-   self:SetIronsights( bIronsights )
-   
-   if SERVER then
-      self:SetZoom(bIronsights)
-   end
-   
-   self.Weapon:SetNextSecondaryFire(CurTime() + 0.3)
+	if not self.IronSightsPos then return end
+	if self.Weapon:GetNextSecondaryFire() > CurTime() then return end
+	
+	bIronsights = not self:GetIronsights()
+	
+	self:SetIronsights( bIronsights )
+	
+	if SERVER then
+		self:SetZoom(bIronsights)
+	end
+	
+	self.Weapon:SetNextSecondaryFire(CurTime() + 0.3)
 end
 
 function SWEP:PreDrop()
-   self:SetZoom(false)
-   self:SetIronsights(false)
-   return self.BaseClass.PreDrop(self)
+	self:SetZoom(false)
+	self:SetIronsights(false)
+	return self.BaseClass.PreDrop(self)
 end
 
 function SWEP:Reload()
-   self.Weapon:DefaultReload( ACT_VM_RELOAD );
-   self:SetIronsights( false )
-   self:SetZoom(false)
+	self.Weapon:DefaultReload( ACT_VM_RELOAD );
+	self:SetIronsights( false )
+	self:SetZoom(false)
 end
 
 
 function SWEP:Holster()
-   self:SetIronsights(false)
-   self:SetZoom(false)
-   return true
+	self:SetIronsights(false)
+	self:SetZoom(false)
+	return true
 end
 
 if CLIENT then
-   function SWEP:DrawHUD()
-      if self:GetIronsights() then
-      else
-         return self.BaseClass.DrawHUD(self)
-      end
-   end
+	function SWEP:DrawHUD()
+		if self:GetIronsights() then
+		else
+			return self.BaseClass.DrawHUD(self)
+		end
+	end
 end
--- garrysmod/gamemodes/terrortown/entities/weapons/weapon_ttt_rbull/shared.lua
+++ garrysmod/gamemodes/terrortown/entities/weapons/weapon_ttt_rbull/shared.lua
@@ -1,26 +1,32 @@
+-- Taurus Raging Bull
+
 if SERVER then
 	AddCSLuaFile( "shared.lua" )
 	resource.AddFile("materials/VGUI/ttt/icon_bb_rbull.vmt")
 	resource.AddFile("models/weapons/v_revl_raging.mdl")
 	resource.AddFile("models/weapons/w_revl_raging.mdl")
-	resource.AddFile("materials\models\weapons\v_models\v_revl_raging\barrel.vmt")
-	resource.AddFile("materials\models\weapons\v_models\v_revl_raging\body.vmt")  
-	resource.AddFile("materials\models\weapons\v_models\v_revl_raging\bullet.vmt")
+	resource.AddFile( "materials\models\weapons\v_models\v_revl_raging\barrel.vmt" )
+	resource.AddFile( "materials\models\weapons\v_models\v_revl_raging\barrel.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_revl_raging\body.vmt" )
+	resource.AddFile( "materials\models\weapons\v_models\v_revl_raging\body.vtf" )
+	resource.AddFile( "materials\models\weapons\v_models\v_revl_raging\bullet.vmt" )
+	resource.AddFile( "materials\models\weapons\v_models\v_revl_raging\bullet.vtf" )
 	resource.AddFile("sound\weapons\ragingbull\bulldraw.wav")
 	resource.AddFile("sound\weapons\ragingbull\bullreload.wav")
-	resource.AddFile("sound\weapons\ragingbull\revolver.wav")	
+	resource.AddFile("sound\weapons\ragingbull\revolver.wav")
+	resource.AddFile("scripts/sounds/ragingbull.txt")
 end
-   
+	
 SWEP.HoldType			= "pistol"
 
 if CLIENT then
-   SWEP.PrintName			= "Taurus Raging Bull"			
-   SWEP.Author				= "EraYaN"
+	SWEP.PrintName			= "Taurus Raging Bull"			
+	SWEP.Author				= "EraYaN"
 
-   SWEP.Slot				= 1
-   SWEP.SlotPos			= 1
+	SWEP.Slot				= 1
+	SWEP.SlotPos			= 1
 
-   SWEP.Icon = "VGUI/ttt/icon_bb_rbull"
+	SWEP.Icon = "VGUI/ttt/icon_bb_rbull"
 end
 
 SWEP.Base				= "weapon_tttbase"
@@ -30,7 +36,7 @@ SWEP.AdminSpawnable = true
 
 SWEP.Kind = WEAPON_PISTOL
 
-SWEP.Primary.Ammo       = "AlyxGun" -- hijack an ammo type we don't use otherwise
+SWEP.Primary.Ammo		 = "AlyxGun" -- hijack an ammo type we don't use otherwise
 SWEP.Primary.Recoil			= 6
 SWEP.Primary.Damage = 45
 SWEP.Primary.Delay = 0.7
@@ -42,7 +48,7 @@ SWEP.Primary.Automatic = true
 
 SWEP.HeadshotMultiplier = 4.5
 
-SWEP.AutoSpawnable      = true
+SWEP.AutoSpawnable		= true
 SWEP.AmmoEnt = "item_ammo_revolver_ttt"
 SWEP.Primary.Sound			= Sound( "weapons/ragingbull/revolver.wav" )
 SWEP.ViewModel			= "models/weapons/v_revl_raging.mdl"
